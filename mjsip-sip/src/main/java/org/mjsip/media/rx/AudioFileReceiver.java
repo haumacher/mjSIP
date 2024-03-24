11efb47450f2a2325046ca15b868040d641ee2eb
Bernhard Haumacher <haui@haumacher.de>
2024-03-24 11:41:31 +0100

    Fixed typo in log message.

--- mjsip-sip/src/main/java/org/mjsip/media/rx/AudioFileReceiver.java
+++ mjsip-sip/src/main/java/org/mjsip/media/rx/AudioFileReceiver.java
@@ -39,7 +39,7 @@ public class AudioFileReceiver implements AudioReceiver {
 			CodecType codec,
 			int payload_type, RtpPayloadFormat payloadFormat, int sample_rate, int channels, Encoder additional_decoder, RtpStreamReceiverListener listener)
 			throws IOException, UnsupportedAudioFileException {
-		LOG.info("Storing audio stram to file " + _audioFile + " format: " + audio_format);
+		LOG.info("Storing audio stream to file " + _audioFile + " format: " + audio_format);
 		OutputStream output_stream = AudioFile.getAudioFileOutputStream(_audioFile, audio_format);
 		RtpStreamReceiver receiver = new RtpStreamReceiver(options, output_stream, additional_decoder, payloadFormat,
 				socket, listener) {
