e5ded7aefc086aa5ff81ce9b31611f5f1262b75d
Bernhard Haumacher <haui@haumacher.de>
2024-03-24 11:47:21 +0100

    Added rudimentary test case for AudioApp.

---
+++ mjsip-examples/src/test/java/org/mjsip/examples/TestAudioApp.java
@@ -0,0 +1,30 @@
+package org.mjsip.examples;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test case for {@link AudioApp}.
+ */
+public class TestAudioApp {
+	
+	@Test
+	public void testSendReceive() throws IOException {
+		File rx = File.createTempFile("rx-audio", ".wav");
+		
+		AudioApp.main(new String[] {
+			"-r" , "5000",
+			"-s", "localhost:5000",
+			"-i", new File("./src/test/fixtures/tone-ulaw-8000khz.wav").getAbsolutePath(),
+			"-o", rx.getAbsolutePath(),
+			"--stopWhenTransmitted"
+		});
+		
+		assertTrue(rx.length() > 10000);
+	}
+
+}
