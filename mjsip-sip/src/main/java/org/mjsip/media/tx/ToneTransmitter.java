db146ca988a18a6e6e657ec5df57a3aec5e545cb
Bernhard Haumacher <haui@haumacher.de>
2025-02-24 21:08:38 +0100

    Convert tone output to requested target audio format.

--- mjsip-sip/src/main/java/org/mjsip/media/tx/ToneTransmitter.java
+++ mjsip-sip/src/main/java/org/mjsip/media/tx/ToneTransmitter.java
@@ -6,6 +6,7 @@ package org.mjsip.media.tx;
 import java.io.IOException;
 
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 
 import org.mjsip.media.RtpStreamSender;
 import org.mjsip.media.RtpStreamSenderListener;
@@ -16,6 +17,7 @@ import org.mjsip.sound.ToneInputStream;
 import org.slf4j.LoggerFactory;
 import org.zoolu.net.UdpSocket;
 import org.zoolu.sound.CodecType;
+import org.zoolu.sound.ConverterAudioSystem;
 import org.zoolu.util.Encoder;
 
 /**
@@ -26,13 +28,10 @@ public class ToneTransmitter implements AudioTransmitter {
 	private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ToneTransmitter.class);
 
 	/** Test tone frequency [Hz] */
-	public static int TONE_FREQ = 100;
+	public static int TONE_FREQ = 330;
 
 	/** Test tone amplitude (from 0.0 to 1.0) */
-	public static double TONE_AMPL = 1.0;
-
-	/** Test tone sample size [bits] */
-	public static final int TONE_SAMPLE_SIZE = 8;
+	public static double TONE_AMPL = 0.8;
 
 	/** By default whether using big-endian rule for byte ordering */
 	public static final boolean DEFAULT_BIG_ENDIAN = false;
@@ -62,9 +61,18 @@ public class ToneTransmitter implements AudioTransmitter {
 			RtpPayloadFormat payloadFormat, int sample_rate, int channels, Encoder additional_encoder, long packet_time,
 			int packet_size, String remote_addr, int remote_port, RtpStreamSenderListener listener, RtpControl rtpControl) throws IOException {
 		LOG.info("Tone generator: {} Hz", _toneFreq);
-		ToneInputStream audioIn = new ToneInputStream(_toneFreq, _toneAmpl, sample_rate, TONE_SAMPLE_SIZE,
-				ToneGenerator.Encoding.PCM_LINEAR_UNSIGNED, DEFAULT_BIG_ENDIAN);
-		RtpStreamSender sender = new RtpStreamSender(options, audioIn, true, payload_type, payloadFormat,
+
+		int sampleSizeInBits = audio_format.getSampleSizeInBits();
+		ToneInputStream sinWave = new ToneInputStream(_toneFreq, _toneAmpl, sample_rate,
+				(sampleSizeInBits + 7) / 8,
+				ToneGenerator.Encoding.PCM_LINEAR_SIGNED, DEFAULT_BIG_ENDIAN);
+
+		AudioInputStream tone = new AudioInputStream(sinWave,
+				new AudioFormat(sample_rate, sampleSizeInBits, 1, true, DEFAULT_BIG_ENDIAN), 65000);
+
+		AudioInputStream converter = ConverterAudioSystem.convertAudioInputStream(codec, sample_rate, tone);
+
+		RtpStreamSender sender = new RtpStreamSender(options, converter, true, payload_type, payloadFormat,
 				sample_rate, channels, packet_time, packet_size, additional_encoder, udp_socket, remote_addr,
 				remote_port, rtpControl, listener);
 		return new RtpAudioTxHandle(sender);
